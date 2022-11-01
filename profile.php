23736dff3924c0634eb423f140ed36a3102cd4e8
Aliya irfani <113844636+Aliyairfani@users.noreply.github.com>
2022-11-01 14:28:55 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION["login"])) {
 
   <link rel="stylesheet" href="css/style.css?v=<?= time(); ?>">
 
-  <title> Contact | Nama Toko </title>
+  <title> Profile | Nama Toko </title>
 </head>
 
 <body>
@@ -38,9 +38,59 @@ if (!isset($_SESSION["login"])) {
     <div class="main-content">
 
       <section class="wrapper">
+        <!-- ISI DI SINI -->
+        <form action="" method="POST" enctype="multipart/form-data">
+            <table cellspacing="20">
 
-        ISI DI SINI
-      </section>
+              <!-- username -->
+              <tr>
+                <td> <label for="username"> Username* </label> </td>
+                <td><center> : </center></td>
+                <td> <input type="text" name="username" id="username" placeholder="Username" class="form-input" autocomplete="off" required> </td>
+              </tr>
+
+              <!-- password -->
+              <tr>
+                <td> <label for="password"> Password* </label> </td>
+                <td><center> : </center></td>
+                <td> <input type="password" name="password" id="password" placeholder="Password" class="form-input" autocomplete="off" required> </td>
+              </tr>
+              <!-- konfirmasi password -->
+              <tr>
+                <td> <label for="konfirmasi"> Konfirmasi Password* </label> </td>
+                <td><center> : </center></td>
+                <td> <input type="password" name="konfirmasi" id="konfirmasi" placeholder="Konfirmasi Password" class="form-input" autocomplete="off" required> </td>
+              </tr>
+              
+              <!-- telepon -->
+              <tr>
+                <td> <label for="telepon"> No. Telepon* </label> </td>
+                <td><center> : </center></td>
+                <td> <input type="text" name="telepon" id="telepon" placeholder="No. Telepon" class="form-input" autocomplete="off" required onkeypress="return numOnly(event)" maxlength="15"> </td>
+              </tr>
+              
+              <!-- alamat -->
+              <tr>
+                <td> <label for="alamat"> Alamat* </label> </td>
+                <td><center> : </center></td>
+                <td> <textarea name="alamat" id="alamat" cols="25" rows="5" placeholder="Alamat" class="form-input" autocomplete="off" required maxlength="100"></textarea> </td>
+              </tr>
+
+              <!-- gambar -->
+              <tr>
+                <td> <label for="gambar"> Foto Profil </label> </td>
+                <td><center> : </center></td>
+                <td> <input type="file" name="gambar" id="gambar" accept="image/*" class="form-input" value="img/plugins/user.png"> </td>
+              </tr>
+
+              <!-- submit -->
+              <tr>
+                <td colspan="3"> <input type="submit" value="Update Profile" name="sign_up" class="btn-block"> </td>
+              </tr>
+
+            </table>
+          </form>
+        </section>
 
     </div>
 
