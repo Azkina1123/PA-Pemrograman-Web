89e5a65096375883575b7bbf523580cb40687bd3
Aliya irfani <113844636+Aliyairfani@users.noreply.github.com>
2022-11-01 15:39:02 +0800

    about, contact, profile

--- contact.php
+++ contact.php
@@ -27,12 +27,36 @@
       <section class="wrapper">
         <!-- ISI DI SINI -->
         <h2>Alamat</h2>
-        <p>Jalan. Mawar no. 12</p><br>
+        <p>Jalan. Mawar RT. 04 No. 78 Kec. Tulip Biru Kota Anggrek Plant World </p><br>
+
+        <!-- <h1 class="title-brown-bg">CONTACT</h1>
+    <section class="contact">
+        <div class="row-contact">
+            <div class="contact-col">
+                <div class="card-contact">
+                    <a href=" https://www.instagram.com/mchls._"><img src="image/instagram.png"></a>
+                    <h3 class="title-white-bg">Instagram</h3>
+                </div>
+            </div>
+            <div class="contact-col">
+                <div class="card-contact">
+                    <a href="https://api.whatsapp.com/send/?phone=6281350502003&text&type=phone_number&app_absent=0"><img src="image/whatsapp.png"></a>
+                    <h3 class="title-white-bg">WhatsApp</h3>
+                </div>
+            </div>
+            <div class="contact-col">
+                <div class="card-contact">
+                    <a href="https://youtu.be/dQw4w9WgXcQ"><img src="image/youtube.png"></a>
+                    <h3 class="title-white-bg">Youtube</h3>
+                </div>
+            </div>
+        </div>
+    </section> -->
 
         <h2>Media Sosial</h2>
-        <h3>Instagram</h3> <p>@Green_Feel_Florist</p>
-        <h3>Whatsapp</h3> <p>0812-3456-7890</p>
-        <h3>Email</h3> <p>Greenfeelflorist@gmail.com</p>
+        <img src ="img/icon web/instagram.png" width = 100px><p>@Green_Feel_Florist</p>
+        <img src ="img/icon web/whatsapp.png" width = 100px> <p>0812-3456-7890</p>
+        <img src ="img/icon web/email.png" width = 110px> <p>Greenfeelflorist@gmail.com</p>
       </section>
 
     </div>
