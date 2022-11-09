1eb93939c9211241c445a41385b1aee1a820c66a
KekenZ <aoshofrina@gmail.com>
2022-11-09 13:02:47 +0800

    Update add.php

--- add.php
+++ add.php
@@ -34,12 +34,12 @@ if (isset($_POST['tambah'])) {
   $tinggi = $_POST['tinggi'];
   $berat = $_POST['berat'];
   $deskripsi = $_POST['deskripsi'];
-  
+
   // buat id, ganti nama gambar
   $ids = $db->query("SELECT id FROM produk");
-  $id_baru = mysqli_num_rows($ids)+1;
+  $id_baru = mysqli_num_rows($ids) + 1;
   $gambar = "product-$id_baru.png";
-  
+
   // jika mengupload gambar, pindahkan direktori
   if ($_FILES["gambar"]["error"] !== 4) {
 
@@ -50,12 +50,12 @@ if (isset($_POST['tambah'])) {
 
     // buat nama file
     $gambar = "product-$id_baru.$ekstensi"; // agar tidak ada nama file yang sama
-    
+
     // pindahkan ke direktori img/products
     $tmp = $_FILES['gambar']['tmp_name'];
-    move_uploaded_file($tmp, "img/products/".$gambar);
-  
-  // jika tidak mengupload gambar, set gambar default
+    move_uploaded_file($tmp, "img/products/" . $gambar);
+
+    // jika tidak mengupload gambar, set gambar default
   } else {
     copy("img/icons/product.png", "img/products/$gambar");
   }
@@ -73,7 +73,7 @@ if (isset($_POST['tambah'])) {
           document.location.href = 'index.php';
       </script>";
 
-  // jika gagal disimpan ke db
+    // jika gagal disimpan ke db
   } else {
     echo "
       <script>
@@ -85,9 +85,7 @@ if (isset($_POST['tambah'])) {
 // edit produk
 if (isset($_POST["edit"])) {
 
-
   // jika sudah mengedit
-  if (isset($_POST['kirim'])) {
     $nama = $_POST['nama'];
     $jenis = $_POST['jenis'];
     $harga = $_POST['harga'];
@@ -133,7 +131,7 @@ if (isset($_POST["edit"])) {
     if ($result) {
       echo "<script>
               alert('Produk Berhasil Diperbarui');
-              document.location.href = 'products.php?mode=edit';
+              document.location.href = 'product.php?id=$id';
           </script>";
 
       // jika gagal update
@@ -142,7 +140,6 @@ if (isset($_POST["edit"])) {
                 alert('Produk Gagal Diperbarui');
             </script>";
     }
-  }
   
 }
 
@@ -159,18 +156,18 @@ if (isset($_POST["edit"])) {
   <link rel="stylesheet" href="css/style.css?v=<?= time(); ?>">
   <link rel="stylesheet" href="css/add.css?v=<?= time(); ?>">
 
-  <title> Add Product | Green Florist </title>
+  <title> <?= $mode == "add" ? "Add Product" : "Edit Product" ?> | Green Florist </title>
 </head>
 
 <body>
   <div class="page-wrapper home-page admin add">
 
-    <header class="img" style="background-image: url('img/header-add.jpg');">
+    <header class="img" style="background-image: url('img/header-<?= $mode; ?>.jpg');">
 
       <?php require "nav.php"; ?>
 
       <div class="banner mini center">
-        <h1> ADD PRODUCT </h1>
+        <h1> <?= $mode == "add" ? "ADD PRODUCT" : "EDIT PRODUK"; ?> </h1>
 
       </div>
 
@@ -180,94 +177,107 @@ if (isset($_POST["edit"])) {
 
       <section class="wrapper">
 
-        <form action = "" method = "post" enctype = "multipart/form-data" class="center">
+        <form action="" method="post" enctype="multipart/form-data" class="center">
 
-          <h1><center> Tambahkan Produk Baru </center></h1> <hr>
+          <h1>
+            <center> <?= $mode == "add" ? "Tambahkan Produk Baru" : $row["nama"]; ?> </center>
+          </h1>
+          <hr>
 
           <table>
             <!-- tanggal -->
             <tr>
-              <td><label for = "tanggal"> TANGGAL RILIS* </label></td>
-              <td><center>:</center></td>
-              <td><input type = "date" name = "tanggal" class="form-input" required id="tanggal" 
-              value="<?= $mode == "add" ? date("Y-m-d") : $row["tanggal_rilis"]; ?>" readonly></td>
+              <td><label for="tanggal"> TANGGAL RILIS* </label></td>
+              <td>
+                <center>:</center>
+              </td>
+              <td><input type="date" name="tanggal" class="form-input" required id="tanggal" value="<?= $mode == "add" ? date("Y-m-d") : $row["tanggal_rilis"]; ?>" readonly></td>
             </tr>
 
             <!-- nama tanaman -->
             <tr>
-              <td><label for = "nama">NAMA TANAMAN*</label></td>
-              <td><center>:</center></td>
-              <td><input type = "text" name = "nama" class="form-input" required id="nama"
-              value="<?= $mode == "add" ? "" : $row["nama"]; ?>"></td>
+              <td><label for="nama">NAMA TANAMAN*</label></td>
+              <td>
+                <center>:</center>
+              </td>
+              <td><input type="text" name="nama" class="form-input" required id="nama" value="<?= $mode == "add" ? "" : $row["nama"]; ?>"></td>
             </tr>
 
             <tr>
-              <td> <label for = "jenis">JENIS TANAMAN*</label> </td>
-              <td><center>:</center></td>
+              <td> <label for="jenis">JENIS TANAMAN*</label> </td>
               <td>
-                <input type = "radio" name = "jenis" value = "tanaman hias" id="tanaman-hias" 
-                <?= $mode == "edit" && ($row['jenis'] == "tanaman hias") ? "checked" : "" ?> 
-                <?= $mode == "add" ? "checked" : "" ?>> 
+                <center>:</center>
+              </td>
+              <td>
+                <input type="radio" name="jenis" value="tanaman hias" id="tanaman-hias" <?= $mode == "edit" && ($row['jenis'] == "tanaman hias") ? "checked" : "" ?> <?= $mode == "add" ? "checked" : "" ?>>
                 <label for="tanaman-hias"> Tanaman Hias </label> <br>
 
-                <input type = "radio" name = "jenis" value = "tanaman buah" id="tanaman-buah"
-                <?=  $mode == "edit" && ($row['jenis'] == "tanaman buah") ? "checked" : "" ?>>
+                <input type="radio" name="jenis" value="tanaman buah" id="tanaman-buah" <?= $mode == "edit" && ($row['jenis'] == "tanaman buah") ? "checked" : "" ?>>
                 <label for="tanaman-buah"> Tanaman Buah </label> <br>
 
-                <input type = "radio" name = "jenis" value = "benih tanaman" id="benih-tanaman"
-                <?=  $mode == "edit" && ($row['jenis'] == "benih tanaman") ? "checked" : "" ?>> 
+                <input type="radio" name="jenis" value="benih tanaman" id="benih-tanaman" <?= $mode == "edit" && ($row['jenis'] == "benih tanaman") ? "checked" : "" ?>>
                 <label for="benih-tanaman"> Benih Tanaman </label> <br>
               </td>
             </tr>
 
             <tr>
-              <td><label for = "harga">HARGA*</label></td>
-              <td><center>:</center></td>
-              <td><input type = "number" name = "harga" class="form-input" required id="harga"
-              value="<?= $mode == "add" ? "" : $row["harga"]; ?>"></td>
+              <td><label for="harga">HARGA*</label></td>
+              <td>
+                <center>:</center>
+              </td>
+              <td><input type="number" name="harga" class="form-input" required id="harga" value="<?= $mode == "add" ? "" : $row["harga"]; ?>"></td>
             </tr>
 
             <tr>
-              <td><label for = "stok">STOK*</label></td>
-              <td><center>:</center></td>
-              <td><input type = "number" name = "stok" class="form-input" min="0" required id="stok"
-              value="<?= $mode == "add" ? "" : $row["stok"]; ?>"></td>
+              <td><label for="stok">STOK*</label></td>
+              <td>
+                <center>:</center>
+              </td>
+              <td><input type="number" name="stok" class="form-input" min="0" required id="stok" value="<?= $mode == "add" ? "" : $row["stok"]; ?>"></td>
             </tr>
 
             <tr>
-              <td><label for = "tinggi">TINGGI*</label></td>
-              <td><center>:</center></td>
-              <td><input type = "float" name = "tinggi" class="form-input" required min="0" id="tinggi"
-              value="<?= $mode == "add" ? "" : $row["tinggi"]; ?>"></td>
+              <td><label for="tinggi">TINGGI*</label></td>
+              <td>
+                <center>:</center>
+              </td>
+              <td><input type="float" name="tinggi" class="form-input" required min="0" id="tinggi" value="<?= $mode == "add" ? "" : $row["tinggi"]; ?>"></td>
             </tr>
 
             <tr>
-              <td><label for = "berat">BERAT*</label></td>
-              <td><center>:</center></td>
-              <td><input type = "float" name = "berat" class="form-input" required min="0" id="berat"
-              value="<?= $mode == "add" ? "" : $row["berat"]; ?>"></td>
+              <td><label for="berat">BERAT*</label></td>
+              <td>
+                <center>:</center>
+              </td>
+              <td><input type="float" name="berat" class="form-input" required min="0" id="berat" value="<?= $mode == "add" ? "" : $row["berat"]; ?>"></td>
             </tr>
 
             <tr>
-              <td><label for = "deskripsi">DESKRIPSI TANAMAN*</label></td>
-              <td><center>:</center></td>
-              <td><textarea name = "deskripsi" class="form-input" required id="deskripsi"><?= $mode == "add" ? "" : $row["deskripsi"]; ?></textarea></td>
+              <td><label for="deskripsi">DESKRIPSI TANAMAN*</label></td>
+              <td>
+                <center>:</center>
+              </td>
+              <td><textarea name="deskripsi" class="form-input" required id="deskripsi"><?= $mode == "add" ? "" : $row["deskripsi"]; ?></textarea></td>
             </tr>
 
             <tr>
               <td><label for="gambar">GAMBAR TANAMAN</label></td>
-              <td><center>:</center></td>
+              <td>
+                <center>:</center>
+              </td>
               <td>
                 <?php if ($mode == "edit") { ?>
                   <img src="img/products/<?= $row["gambar"]; ?>" alt="" height="200"> <br>
                 <?php } ?>
-                <input type="file" name = "gambar" class="form-input" id="gambar">
+                <input type="file" name="gambar" class="form-input" id="gambar">
+                <input type="text" name="gambar_lama" value="<?= $row["gambar"]; ?>" hidden>
               </td>
             </tr>
 
             <tr>
-              <td colspan="3"><center><button a class = "links btn-block" 
-              name = "<?= $mode == "add" ? "tambah" : "edit"; ?>" href = "">Submit</button></center></td>
+              <td colspan="3">
+                <center><button type="submit" class="btn-block" name="<?= $mode == "add" ? "tambah" : "edit"; ?>">Submit</button></center>
+              </td>
             </tr>
 
           </table>
